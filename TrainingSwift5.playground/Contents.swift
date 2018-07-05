da03910740b4447e95cca3cab2428bc45e3f62dc
hung070996 <do.tien.hung@framgia.com>
2018-07-05 14:56:58 +0700

    array

---
+++ TrainingSwift5.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var arr1 = [Int]()
+var arr2 = Array(repeating: 2, count: 5)
+var arr3 = Array(repeating: 1, count: 3)
+var arr4 = arr2 + arr3
+var arr5 : [String] = ["a", "b", "c"]
